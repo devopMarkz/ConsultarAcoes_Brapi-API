422a2715b895b076260a3cc506470e0f5db10520
devopMarkz <marcosdev2002@gmail.com>
2024-12-07 20:40:16 -0300

    att

--- src/main/java/com/marcos/build_and_run/repositories/UsuarioRepository.java
+++ src/main/java/com/marcos/build_and_run/repositories/UsuarioRepository.java
@@ -3,10 +3,11 @@ package com.marcos.build_and_run.repositories;
 import com.marcos.build_and_run.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    @Query("SELECT p FROM Usuario p JOIN FETCH p.contas")
-    public Usuario findUsuarioByIdWithConta(Long id);
+    @Query("SELECT u FROM Usuario u JOIN FETCH u.contas WHERE u.id = :id")
+    public Usuario findUsuarioByIdWithConta(@Param("id") Long id);
 
 }
